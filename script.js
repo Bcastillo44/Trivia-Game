b644275a0514de03448f958f65dce109bf4047c8
B CASTILLO <bryancastillo44@outlook.com>
2020-05-13 22:58:08 -0400

    Added modal CSS functionality

--- script.js
+++ script.js
@@ -1,84 +1,187 @@
 
+const modalTriggerButton = document.querySelector("button");
+const modal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal-close");
 
-var questions = [
-     {
-           prompt: "What is Monica’s biggest pet peeve?\n(A) Animals dressed as humans\n\ (B) Rearranging her furniture\n(C) Dirty apartments",
-           answer: "a"
-     },
-     {
-          prompt: "According to Chandler, what phenomenon scares the bajesus out of him?\n(A) Clowns\n\ (B) Goldfish Crackers\n(C) Michael Flatley Lord of the Dance",
-          answer: "c"
-     },
-     {
-          prompt: "Who pees on Monica’s leg when she gets stung by a jellyfish?\n(A) Joey\n\ (B) Ross\n(C) Chandler",
-          answer: "a"
-     }
-];
+modalTriggerButton.addEventListener("click", event => toggleModal());
 
-var indexCardOne = document.querySelector(".card_one_fears");
+modalCloseButton.addEventListener("click", event => toggleModal());
 
-indexCardOne.addEventListener("click", runQuestions, false);
+modal.addEventListener("click", event => {
+	if(event.currentTarget===event.target) toggleModal();
+});
 
-function runQuestions(e){
-	var clickedItem = e.target.element;
-	for(var i = 0; i < questions.length; i++){
-     var response = window.prompt(questions[i].prompt);
-     if(response == questions[i].answer){
-          score++;
-          alert("Correct!");
-     } else {
-          alert("Sorry, that's incorrect!");
-     }
+function toggleModal() {
+	if(window.getComputedStyle(modal).display ==="flex"){
+		modal.classList.add("modal-hide");
+		setTimeout(() => {
+			modal.style.display = "none";
+			modal.classList.remove("modal-show", "modal-hide");
+			document.body.style.overflow = "initial";
+		}, 200);
+		
+	}
+	else{
+		modal.style.display = "flex";
+		modal.classList.add("modal-show");
+		document.body.style.overflow = "hidden";
+	}
 }
-alert("you got " + score + "/" + questions.length);
+
+
+// var questions = [
+//      {
+//            prompt: "What is Monica’s biggest pet peeve?\n(A) Animals dressed as humans\n\ (B) Rearranging her furniture\n(C) Dirty apartments",
+//            answer: "a"
+//      },
+//      {
+//           prompt: "According to Chandler, what phenomenon scares the bajesus out of him?\n(A) Clowns\n\ (B) Goldfish Crackers\n(C) Michael Flatley Lord of the Dance",
+//           answer: "c"
+//      },
+//      {
+//           prompt: "Who pees on Monica’s leg when she gets stung by a jellyfish?\n(A) Joey\n\ (B) Ross\n(C) Chandler",
+//           answer: "a"
+//      }
+// ];
+
+// var indexCardOne = document.querySelector(".card_one_fears_btn");
+
+// indexCardOne.addEventListener("click", runQuestions, false);
+
+// function runQuestions(e){
+// 	var clickedItem = e.target.element;
+// 	for(var i = 0; i < questions.length; i++){
+//      var response = window.prompt(questions[i].prompt);
+//      if(response == questions[i].answer){
+//           score++;
+//           alert("Correct!");
+//      } else {
+//           alert("Sorry, that's incorrect!");
+//      }
+// }
+// alert("you got " + score + "/" + questions.length);
 	
-}
-// console.log(questions);
+// }
+// // console.log(questions);
 
-var score = 0;
-
-
+// var score = 0;
 
 
-var questions = [
-     {
-           prompt: "What is the name of Ross’ first wife?\n(A) Emily\n\ (B) Susan\n(C) Carol",
-           answer: "c"
-     },
-     {
-          prompt: "What was the original name of the show?\n(A) Central Perk\n\ (B) Insomnia Cafe\n(C) Monk's Cafe",
-          answer: "b"
-     },
-     {
-          prompt: "Who was Rachel supposed to marry in the pilot episode?\n(A) Barry\n\ (B) Larry\n(C) Joshua",
-          answer: "a"
-     }
-];
 
-var indexCardOne = document.querySelector(".card_one_ancient");
 
-indexCardOne.addEventListener("click", runQuestions, false);
+// var questions = [
+//      {
+//            prompt: "What is the name of Ross’ first wife?\n(A) Emily\n\ (B) Susan\n(C) Carol",
+//            answer: "c"
+//      },
+//      {
+//           prompt: "What was the original name of the show?\n(A) Central Perk\n\ (B) Insomnia Cafe\n(C) Monk's Cafe",
+//           answer: "b"
+//      },
+//      {
+//           prompt: "Who was Rachel supposed to marry in the pilot episode?\n(A) Barry\n\ (B) Larry\n(C) Joshua",
+//           answer: "a"
+//      }
+// ];
 
-function runQuestions(e){
-	var clickedItem = e.target.element;
-	for(var i = 0; i < questions.length; i++){
-     var response = window.prompt(questions[i].prompt);
-     if(response == questions[i].answer){
-          score++;
-          alert("Correct!");
-     } else {
-          alert("Sorry, that's incorrect!");
-     }
-}	
-	alert("You got " + score + "/" + questions.length);
+// var indexCardOne = document.querySelector(".card_one_ancient");
+
+// indexCardOne.addEventListener("click", runQuestions, false);
+
+// function runQuestions(e){
+// 	var clickedItem = e.target.element;
+// 	for(var i = 0; i < questions.length; i++){
+//      var response = window.prompt(questions[i].prompt);
+//      if(response == questions[i].answer){
+//           score++;
+//           alert("Correct!");
+//      } else {
+//           alert("Sorry, that's incorrect!");
+//      }
+// }	
+// 	alert("You got " + score + "/" + questions.length);
 	
-}
-// console.log(questions);
+// }
+// // console.log(questions);
 
-var score = 0;
+// var score = 0;
 
 
 
+// var questions = [
+//      {
+//            prompt: "Every week the TV Guide comes to Chandler and Joey’s apartment, what name appears on the address label?\n(A) Joey Tribiani\n\ (B) Chandler Bing\n(C) Miss Chanandler Bong",
+//            answer: "c"
+//      },
+//      {
+//           prompt: "What was the name of the self-help book that the girls loved?\n(A) Be Your Own Windkeeper\n\ (B) Lady in the Wind\n(C) Stand Up For Yourself",
+//           answer: "a"
+//      },
+//      {
+//           prompt: "What book did Joey keep in the freezer?\n(A) IT\n\ (B) Little Women\n(C) The Shining",
+//           answer: "c"
+//      }
+// ];
+
+// var indexCardOne = document.querySelector(".card_one_literature");
+
+// indexCardOne.addEventListener("click", runQuestions, false);
+
+// function runQuestions(e){
+// 	var clickedItem = e.target.element;
+// 	for(var i = 0; i < questions.length; i++){
+//      var response = window.prompt(questions[i].prompt);
+//      if(response == questions[i].answer){
+//           score++;
+//           alert("Correct!");
+//      } else {
+//           alert("Sorry, that's incorrect!");
+//      }
+// }	
+// 	alert("You got " + score + "/" + questions.length);
+	
+// }
+// // console.log(questions);
+
+// var score = 0;
+
+
+// var questions = [
+//      {
+//            prompt: "Monica and I had a grandmother that died, you both went to her funeral…. Name that grandmother?\n(A) Gladys\n\ (B) Althea\n(C) Nana",
+//            answer: "b"
+//      },
+//      {
+//           prompt: "What is the name of Chandler’s father’s Las Vegas all-male burlesque?\n(A) Men's Restroom\n\ (B) The Toolbox\n(C) Viva Las Gaygus",
+//           answer: "c"
+//      },
+//      {
+//           prompt: "To get over Richard, what did Monica start making?\n(A) Jam\n\ (B) Pies\n(C) Donuts",
+//           answer: "a"
+//      }
+// ];
+
+// var indexCardOne = document.querySelector(".card_one_relative");
+
+// indexCardOne.addEventListener("click", runQuestions, false);
+
+// function runQuestions(e){
+// 	var clickedItem = e.target.element;
+// 	for(var i = 0; i < questions.length; i++){
+//      var response = window.prompt(questions[i].prompt);
+//      if(response == questions[i].answer){
+//           score++;
+//           alert("Correct!");
+//      } else {
+//           alert("Sorry, that's incorrect!");
+//      }
+// }	
+// 	alert("You got " + score + "/" + questions.length);
+	
+// }
+// // console.log(questions);
+
+// var score = 0;
 
 
 
